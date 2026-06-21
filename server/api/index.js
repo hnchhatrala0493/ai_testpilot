faa3a9fb9ae5461082b80858f17af46f9e3f4858
Himanshu Chhatrala <hiamsnhu.chhatrala@xpertnest.com>
2026-06-21 18:18:05 +0530

    changes

--- server/api/index.js
+++ server/api/index.js
@@ -38,10 +38,12 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`🚀 Server running on http://localhost:${PORT}`);
+    // });
   })
   .catch((err) => {
     console.error("❌ Database connection error:", err.message);
   });
+
+  module.exports = app;
